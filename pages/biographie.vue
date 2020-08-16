54ff40f90e1e2cc23477fa9f0957d4dea49c0e8b
Boris MAURENCE <b.maurence@gmail.com>
2020-08-16 12:15:31 +0200

    Adding Biographie page

---
+++ pages/biographie.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <vue-simple-markdown :source="bio"></vue-simple-markdown>
+  </div>
+</template>
+
+<script>
+import bioQuery from "~/apollo/queries/bio/bio";
+
+export default {
+  data() {
+    return {
+      bio: {},
+    };
+  },
+  apollo: {
+    bio: {
+      prefetch: true,
+      query: bioQuery,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
