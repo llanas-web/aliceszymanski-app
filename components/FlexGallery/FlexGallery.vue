048f14705d2383acb4d3c4d6550465da8ed5cb11
Boris MAURENCE <b.maurence@gmail.com>
2021-02-06 15:27:38 +0100

    FlexMedia no backgroundMedia fix

--- components/FlexGallery/FlexGallery.vue
+++ components/FlexGallery/FlexGallery.vue
@@ -18,12 +18,12 @@
               : '',
         }"
       >
-        <div
-          class="item-container is-fullheight"
-          v-if="galleryItem.backgroundMedia != null"
-        >
+        <div class="item-container is-fullheight">
           <video
-            v-if="galleryItem.backgroundMedia.mime.indexOf('video') != -1"
+            v-if="
+              galleryItem.backgroundMedia != null &&
+              galleryItem.backgroundMedia.mime.indexOf('video') != -1
+            "
             :ref="'mediaSource' + index"
             class="video-content"
           >
@@ -31,10 +31,13 @@
           </video>
           <audio
             :ref="'mediaSource' + index"
-            v-else-if="galleryItem.backgroundMedia.mime.indexOf('audio') != -1"
+            v-else-if="
+              galleryItem.backgroundMedia != null &&
+              galleryItem.backgroundMedia.mime.indexOf('audio') != -1
+            "
             :src="galleryItem.backgroundMedia.url"
           ></audio>
-          <div class="item-text mb-6">
+          <div class="item-text py-6">
             <div class="mb-4" v-if="galleryItem.backgroundMedia != null">
               <button class="button" @click="playBackground(index)">
                 <span class="icon has-text-warning-light">
@@ -52,7 +55,7 @@
                   path: '/musique-de-chambre',
                   hash: '#' + galleryItem.page.url,
                 }"
-                class="has-text-centered has-text-warning-light is-size-4 is-underline"
+                class="has-text-centered has-text-warning-light is-size-4 is-family-secondary"
                 >{{ galleryItem.page.title }}</nuxt-link
               >
             </div>
@@ -74,6 +77,8 @@ export default {
   methods: {
     playBackground(sourceIndex) {
       for (let i = 0; i < this.galleryListItems.length; i++) {
+        console.dir(sourceIndex);
+        console.dir(this.$refs);
         if (sourceIndex === i) {
           if (sourceIndex === this.readingIndex) {
             this.$refs["mediaSource" + i][0].pause();
@@ -82,7 +87,7 @@ export default {
             this.$refs["mediaSource" + i][0].play();
             this.readingIndex = sourceIndex;
           }
-        } else {
+        } else if (this.$refs["mediaSource" + i]) {
           this.$refs["mediaSource" + i][0].pause();
         }
       }
@@ -113,14 +118,33 @@ export default {
   transition: all 1s;
 }
 
+.item-text {
+  width: 100%;
+  //   visibility: hidden;
+  opacity: 0;
+  background: none;
+  position: absolute;
+  transition: all 1s;
+}
+
 @media screen and (min-width: $mobile) {
   .gallery-item:hover {
     flex-grow: 2;
+    .item-text {
+      visibility: visible;
+      opacity: 1;
+      background: rgb(56, 44, 44);
+      background: linear-gradient(
+        0deg,
+        rgba(56, 44, 44, 1) 0%,
+        rgba(113, 113, 113, 0.4878151944371498) 50%,
+        rgba(0, 0, 0, 0) 100%
+      );
+    }
   }
 }
 
 .item-container {
-  position: absoslute;
   width: 100%;
   height: 100%;
   display: flex;
